181b8e1da1630fce701a50b3daacc26091956e4f
charlesNoel <charlesnoel098@gmail.com>
2021-01-26 11:38:15 -0500

    allo React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  const name = 'React';
+  return ( /*JSX*/
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Allo {name} !
         </p>
         <a
           className="App-link"
